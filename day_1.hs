369d494e8b321d3fbe10212751f9296fb1566628
Awais Hussain <awais@wave.com>
2020-03-20 23:05:19 -0400

    Day 1 Part 2

--- day_1.hs
+++ day_1.hs
@@ -7,8 +7,15 @@ count_parens (x:xs) = case x of
                         ')' -> count_parens xs - 1
                         _ -> count_parens xs
 
+track_floor :: String -> Int -> Int -> Int
+track_floor (x:xs) floor step
+    | floor == (-1) = step
+    | x == '(' = track_floor xs (floor + 1) (step + 1)
+    | x == ')' = track_floor xs (floor - 1) (step + 1)
+    | otherwise = track_floor xs floor (step + 1)
+
 main = do
     withFile "day_1.in" ReadMode (\handle -> do
         contents <- hGetContents handle
-        let res = count_parens contents
+        let res = track_floor contents 0 0
         print res)
