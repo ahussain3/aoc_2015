10bd84233dbd4aa0255088009b845a2683da8e2b
Awais Hussain <awais@wave.com>
2020-03-21 13:04:44 -0400

    Day 2 Part 2

--- day_2.hs
+++ day_2.hs
@@ -12,17 +12,27 @@ split (c:cs) delim
 surfaceArea :: [Int] -> Int
 surfaceArea [l, w, h] = 2*l*w + 2*w*h + 2*h*l
 
+volume :: [Int] -> Int
+volume [l, w, h] = l * w * h
+
 excess :: [Int] -> Int
 excess d | (a:b:_) <- sort d = a * b
          | otherwise = 0
 
+shortestAround :: [Int] -> Int
+shortestAround d | (a:b:_) <- sort d = 2 * a + 2 * b
+                 | otherwise = 0
+
 paperNeeded :: [Int] -> Int
 paperNeeded rect = surfaceArea rect + excess rect
+
+ribbonNeeded :: [Int] -> Int
+ribbonNeeded rect = shortestAround rect + volume rect
 
 main = do
     withFile "day_2.in" ReadMode (\handle -> do
         contents <- hGetContents handle
         let inputs = map (\x -> split x 'x') (lines contents)
         let rects = map (map  (\x -> read x::Int)) inputs
-        let res = sum $ map paperNeeded rects
+        let res = sum $ map ribbonNeeded rects
         print res)
---
+++ notes.md
@@ -0,0 +1,11 @@
+# Learning Notes
+
+Haskell is nice when you have a single unit of work, but is a little more
+fiddly when it comes to data manipulation. E.g. there are no 'for' loops so
+I found myself needing to do maps of maps.
+
+
+Questions:
+- Is there a "spread" operator? Such that I can pass a [Int] of length 3
+  into a function that takes Int -> Int -> Int as input?
+- How can I map over the interior items in a list of lists?
