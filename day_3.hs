3ce136d6c60ce7ed959cc816aa3d72632f831ea4
Awais Hussain <awais@wave.com>
2020-03-21 20:44:39 -0400

    Day 3 Part 2

--- day_3.hs
+++ day_3.hs
@@ -3,17 +3,31 @@ import qualified Data.Set as Set
 
 data Point = Point Int Int deriving (Show)
 
-fly :: Point -> String -> Set.Set(Int, Int) -> Int
+fly :: Point -> String -> Set.Set(Int, Int) -> Set.Set(Int, Int)
 fly (Point x y) (d:ds) visited
     | d == '^' = fly (Point x (y+1)) ds (Set.insert (x, y) visited)
     | d == 'v' = fly (Point x (y-1)) ds (Set.insert (x, y) visited)
     | d == '<' = fly (Point (x-1) y) ds (Set.insert (x, y) visited)
     | d == '>' = fly (Point (x+1) y) ds (Set.insert (x, y) visited)
-    | otherwise = Set.size visited
+    | otherwise = visited
+fly _ [] visited = visited
+
+first(x:y:xs) = x : first xs
+first _ = []
+
+second(x:y:xs) = y : second xs
+second _ = []
 
 main = do
     handle <- openFile "day_3.in" ReadMode
     contents <- hGetContents handle
-    let res = fly (Point 0 0) contents (Set.fromList [(0,0)])
+    let santa_path = first contents
+    let robo_path = second contents
+
+    let santa_visits = fly (Point 0 0) santa_path (Set.fromList [(0,0)])
+    let robo_visits = fly (Point 0 0) robo_path (Set.fromList [(0,0)])
+
+    let res = Set.size $ Set.union santa_visits robo_visits
+
     print res
     hClose handle
--- notes.md
+++ notes.md
@@ -4,6 +4,10 @@ Haskell is nice when you have a single unit of work, but is a little more
 fiddly when it comes to data manipulation. E.g. there are no 'for' loops so
 I found myself needing to do maps of maps.
 
+Wow, it really is true that Haskell code runs correctly the first time you
+run it. It's weird to have so little visibility into what the program is
+doing, but then it always spits out the correct answer as soon as you've
+fixed the compiler errors
 
 Questions:
 - Is there a "spread" operator? Such that I can pass a [Int] of length 3
